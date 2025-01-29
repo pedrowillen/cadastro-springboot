34054b778a2a9557a283485a8966a03f49ce1ef1
Pedro Willen <pedrowillen28@gmail.com>
2025-01-28 21:39:46 -0400

    feat and fix: adicionando e usando dependencia lombok; adicionando unique na coluna de email em ninja model

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinja.Missoes;
 
 import dev.java10x.CadastroDeNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
 
     @Id
@@ -20,14 +26,4 @@ public class MissaoModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissaoModel() {
-    }
-
-    public MissaoModel(Long id, String nome_missao, String descricao_missao, char rank_dificuldade, boolean status_missao) {
-        this.id = id;
-        this.nome_missao = nome_missao;
-        this.descricao_missao = descricao_missao;
-        this.rank_dificuldade = rank_dificuldade;
-        this.status_missao = status_missao;
-    }
 }
--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaModel.java
@@ -2,19 +2,26 @@ package dev.java10x.CadastroDeNinja.Ninjas;
 
 import dev.java10x.CadastroDeNinja.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 // Entity transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
+    private String clan;
+    @Column(unique = true)
     private String email;
     private int idade;
 
@@ -22,36 +29,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //FK
     private MissaoModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
